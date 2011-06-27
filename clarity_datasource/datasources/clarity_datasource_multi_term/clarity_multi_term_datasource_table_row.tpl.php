f470b9b5d0b96cd677a1f2e01b8f965794e116ad
dcervantes <dcervantes@imamuseum.org>
2011-06-27 14:56:46 -0400

    Added new data source.

---
+++ clarity_datasource/datasources/clarity_datasource_multi_term/clarity_multi_term_datasource_table_row.tpl.php
@@ -0,0 +1,9 @@
+<tr>
+<td class='user'><?=$data->uid?></td>
+<td class='timestamp'><?=$data->timestamp?></td>
+<td class='value' ><?=$data->value?></td>
+<td class='category'><?=$data->category?></td>
+<td class='annotation'><?=$data->annotation?></td>
+<td class='modified'><?=$data->modified_time?></td>
+<td class='actions'><a class='edit' href='#'>edit</a></td>
+</tr>
