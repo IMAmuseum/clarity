42fc5669ad4fe7f967261162b9b55c6c60288dc1
ebachta <ebachta@imamuseum.org>
2011-07-12 16:09:09 -0400

    Refactored to leverage the assignment table

--- clarity_datasources/datasources/clarity_datasource_float/clarity_float_table.tpl.php
+++ clarity_datasources/datasources/clarity_datasource_float/clarity_float_table.tpl.php
@@ -3,8 +3,6 @@
 <th class='user'>User</th>
 <th class='timestamp'>Timestamp</th>
 <th class='value' >Value</th>
-<th class='category'>Category</th>
-<th class='annotation'>Annotation</th>
 <th class='modified'>Modified</th>
 <th class='actions'>Actions</th>
 </tr>
