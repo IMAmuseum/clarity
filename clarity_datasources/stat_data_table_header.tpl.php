6018bc6885115a5c46e6d6c8a886e057b0f43a1b
ebachta <ebachta@imamuseum.org>
2011-07-28 14:49:03 -0400

    Added a user column

--- clarity_datasources/stat_data_table_header.tpl.php
+++ clarity_datasources/stat_data_table_header.tpl.php
@@ -1,9 +1,11 @@
 <div class='header'>
-<div class='assignment column w5'>
-<div class='w5'>Assignment</div>
+<div class='assignment column w6'>
+<div class='w6'>Assignment</div>
 <div class='clear'></div>
+<div class='assignment column w1'>User</div>
 <div class='assignment column w2'>Date</div>
 <div class='assignment column w2'>Date Modified</div>
+<div class='assignment column w1'>&nbsp;</div>
 </div>
 <?php foreach($ds_header_labels as $labels): ?>
 <div class='column w<?=count($labels['sublabels'])*2?>'>
