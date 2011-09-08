d1f2f8fae6d0c1c1b9a17fe0088d5858de6832cf
ebachta <ebachta@imamuseum.org>
2011-09-08 10:20:19 -0400

    Improved the assignment table

--- clarity_datasources/stat_data_table.tpl.php
+++ clarity_datasources/stat_data_table.tpl.php
@@ -1,7 +1,9 @@
 <div class='stat-data-table'>
 <?= $header_content ?>
 <div class='clear'></div>
+<div class='rows'>
 <?php foreach ($rows as $row_content): ?>
 <?= $row_content ?>
 <?php endforeach; ?>
+</div>
 </div>
