14661ee63e0081047a239c0613c4ccb2e4cfdc7e
ebachta <ebachta@imamuseum.org>
2011-10-20 14:11:53 -0400

    Displaying properties in the assignment table

--- clarity_datasources/stat_data_table_header.tpl.php
+++ clarity_datasources/stat_data_table_header.tpl.php
@@ -1,17 +1,16 @@
-<colgroup span='4'></colgroup>
+<colgroup span='<?=count($assignment_header_labels);?>'></colgroup>
 <colgroup span='<?=count($ds_header_labels);?>'></colgroup>
 <tr class='header-top'>
-<th colspan='4'>Assignment</th>
+<th colspan='<?=count($assignment_header_labels);?>'>Assignment</th>
 <?php foreach($ds_header_labels as $labels):?>
 <th colspan='<?php count($labels['sublabels'])?>'><?=$labels['label']?></th>
 <?php endforeach; ?>
 <th colspan='2'></th>
 </tr>
 <tr class='header-bottom'>
-<th class='assignment'>User</th>
-<th class='assignment'>Date</th>
-<th class='assignment'>Date Modified</th>
-<th class='assignment'>&nbsp;</th>
+<?php foreach($assignment_header_labels as $label):?>
+<th class='assignment'><?=$label;?></th>
+<?php endforeach;?>
 <?php foreach($ds_header_labels as $labels):?>
 <?php foreach($labels['sublabels'] as $label): ?>
 <th><?=$label?></th>
